866512c37a7f064f149767c4f83a2214bcf70d9a
Łukasz Moszczyński <moszczynski.luk@gmail.com>
2017-12-21 11:14:31 +0100

    Add comments in js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//scripts.js
+
+var name = prompt('Enter your name');
+var surname = prompt('Enter your surname');
+//alert('Hello, ' + name + ' ' + surname);
+/*
+Long
+long
+comment
+*/
+console.log ('Hello, ' + name + ' ' +surname);
+
+//end
